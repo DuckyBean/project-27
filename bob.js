36c9c2349dbcd2331521e4229d49274326ce3af5
DuckyBean <72050129+DuckyBean@users.noreply.github.com>
2021-02-06 18:18:52 -0800

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,40 @@
+class bob {
+
+    constructor(x,y,diameter){
+            // constructor function is invoked everytime we create an object from 'this' class! 
+        var options= {
+
+            restitution:1,
+            density:0.8
+
+        }
+
+        this.body = Bodies.circle(x,y,diameter/2,options);
+        World.add(world, this.body);
+        this.r = diameter/2;
+
+    }
+
+    display(){
+
+      
+        var angle = this.body.angle;
+        var pos = this.body.position;
+
+       push();
+       //strokeWeight(2);
+	   //stroke("red");
+	   fill("aqua");
+       translate(pos.x,pos.y);
+       rotate(angle);
+     
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r);
+        pop();
+
+
+    }
+
+
+
+}
